55e4055dfe804a6b2641d100eb741e6baacbfdd0
raduxc44 <raduxc44@gmail.com>
2022-11-15 22:19:39 +0200

    Whenever the searchbar/categ gets opened, the other closes first

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -4,48 +4,59 @@ import BasicJack from '../../images/Whisky/standard-jack.jpeg'
 
 function Nav () {
 
-
     // Applies in and out animations on categories (mobile version only)
 
     let categAnimationDelay:number = 0;
     let searchAnimationDelay:number = 0;
 
-    function openCategMobile () { 
-        let mobileCateg = document.querySelector('.nav-lower') as HTMLElement;
-        let deactivate = () => {
-            mobileCateg.style.removeProperty('display')
-            mobileCateg.classList.toggle('animate__fadeOutLeft')
-            categAnimationDelay = 0
-        }
-        if(mobileCateg.classList.contains('animate__fadeInLeft') && categAnimationDelay === 1) {
-            mobileCateg.classList.toggle('animate__fadeInLeft');
-            mobileCateg.classList.toggle('animate__fadeOutLeft');
-            // setTimeout(deactivate, 1000);
-            setTimeout(deactivate, 1000)
-        }
-        else if(categAnimationDelay === 0 && !mobileCateg.classList.contains('animate__fadeInLeft')) {
-            setTimeout(() => {categAnimationDelay = 1}, 1000)
-            mobileCateg.style.display = 'block'
-            mobileCateg.classList.toggle('animate__fadeInLeft')
-        }
-    }
+    function openMobileAnim (item:string) { 
 
-    function openSearchMobile () {
+        let mobileCateg = document.querySelector('.nav-lower') as HTMLElement;
         let mobileSearch = document.querySelector('.nav-mobile-search-cont') as HTMLElement;
-        let deactivate = () => {
-            mobileSearch.classList.toggle('animate__fadeOutUp')
-            mobileSearch.style.removeProperty('display')
-            searchAnimationDelay = 0;
+
+        let deactivateCateg = () => {
+            mobileCateg.classList.toggle('animate__fadeOutLeft');
+            setTimeout(() => {
+                mobileCateg.style.removeProperty('display')
+                mobileCateg.classList.toggle('animate__fadeOutLeft')
+                categAnimationDelay = 0
+            }, 1000)   
         }
-        if(mobileSearch.classList.contains('animate__fadeInDown') && searchAnimationDelay === 1) {
-            mobileSearch.classList.toggle('animate__fadeInDown')
+        let deactivateSearch = () => {
             mobileSearch.classList.toggle('animate__fadeOutUp')
-            setTimeout(deactivate, 1000);
+            setTimeout(() => {
+                mobileSearch.classList.toggle('animate__fadeOutUp')
+                mobileSearch.style.removeProperty('display')
+                searchAnimationDelay = 0;
+            }, 1000);    
         }
-        else if(searchAnimationDelay === 0 && !mobileSearch.classList.contains('animate__fadeInDown')){
-            setTimeout(() => {searchAnimationDelay = 1}, 1000)
-            mobileSearch.style.display = 'flex'
-            mobileSearch.classList.toggle('animate__fadeInDown')
+        if(item === 'categ') {   
+            if(categAnimationDelay === 1) {
+                categAnimationDelay = 1.5
+                deactivateCateg();
+            }
+            else if(categAnimationDelay === 0) {
+                categAnimationDelay = 0.5
+                deactivateSearch();
+                setTimeout(() => {categAnimationDelay = 1}, 1000)
+                mobileCateg.style.display = 'block'
+                mobileCateg.classList.toggle('animate__fadeInLeft')
+                setTimeout(() => mobileCateg.classList.toggle('animate__fadeInLeft'), 1000)
+            }
+        }
+        else if(item === 'search') {
+            if(searchAnimationDelay === 1) {
+                searchAnimationDelay = 1.5
+                deactivateSearch();
+            }
+            else if(searchAnimationDelay === 0){
+                searchAnimationDelay = 0.5
+                deactivateCateg()
+                setTimeout(() => {searchAnimationDelay = 1}, 1000)
+                mobileSearch.style.display = 'flex'
+                mobileSearch.classList.toggle('animate__fadeInDown');
+                setTimeout(() => mobileSearch.classList.toggle('animate__fadeInDown'), 1000)
+            }
         }
     }
 
@@ -70,11 +81,11 @@ function Nav () {
             </div>
             <div className='nav-lower-mobile'>
                 <div className='nav-mobile-menu'>
-                    <span onClick={openCategMobile} className="material-symbols-outlined">menu</span>
+                    <span onClick={() => {openMobileAnim('categ')}} className="material-symbols-outlined">menu</span>
                     <p>Products</p>
                 </div>
                 <div className='nav-mobile-utilities'>
-                    <span onClick={openSearchMobile} className='material-symbols-outlined search-icon-mobile'>search</span>
+                    <span onClick={() => {openMobileAnim('search')}} className='material-symbols-outlined search-icon-mobile'>search</span>
                 </div>
             </div>
             <div className='nav-mobile-search-cont animate__animated'>
