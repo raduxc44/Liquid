06d807a762b78cedacfd0f38d2d724be6df05da8
raduxc44 <raduxc44@gmail.com>
2022-12-09 08:53:22 +0200

    Added the about component for future use

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "slick-carousel/slick/slick.css"
 import Nav from './components/Nav/Nav';
 import Banner from './components/Banner/Banner';
 import Recommended from './components/Recommended/Recommended';
+import About from './components/About/About';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
     <Nav></Nav>
     <Banner></Banner>
     <Recommended></Recommended>
+    <About></About>
     </>
     
   );  
---
+++ src/components/About/About.tsx
@@ -0,0 +1,12 @@
+import './About.css'
+
+function About () {
+    return(
+        <div className='about-cont'>
+            <p>About Us</p>
+            <p>Liquid is </p>
+        </div>
+    )
+}
+
+export default About
