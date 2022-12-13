46ad0012f699e926dbdec2642f5dabaa6bf314a4
raduxc44 <raduxc44@gmail.com>
2022-12-13 09:47:35 +0200

    Added low-res categories pics

--- src/components/Categories/Categories.tsx
+++ src/components/Categories/Categories.tsx
@@ -5,7 +5,7 @@ function Categories () {
         <div className='categ-cont'>
             <div className='upper-categ'>
                 <div className='categ-card-sm'>
-                    <img className='categ-pic-sm' src={require(`../../images/Spirits/Whisky/low-res/jack-standard.jpg`)} alt={'Jack'}/>
+                    <img className='categ-pic-sm' src={require(`../../images/Champagne/low-res/dom-perignon.jpg`)} alt={'Champagne'}/>
                     <div>
                         <p className='categ-card-title'>Champagne</p>
                         <p>Champagne placeholder text</p>
@@ -17,11 +17,11 @@ function Categories () {
                         <p className='categ-card-title'>Vodka</p>
                         <p>Silent, yet deadly.</p>
                     </div>
-                    <img className='categ-pic-sm' src={require(`../../images/Spirits/Whisky/low-res/jack-standard.jpg`)} alt={'Jack'}/>
+                    <img className='categ-pic-sm' src={require(`../../images/Spirits/Vodka/low-res/absolut.jpg`)} alt={'Vodka'}/>
                     <div className='check-out-btn categ-small-card-check'>Check it out</div>
                 </div>
                 <div className='categ-card-sm'>
-                    <img className='categ-pic-sm' src={require(`../../images/Spirits/Whisky/low-res/jack-standard.jpg`)} alt={'Jack'}/>
+                    <img className='categ-pic-sm' src={require(`../../images/Spirits/Rum/low-res/the-kraken.jpg`)} alt={'Rum'}/>
                     <div>
                         <p className='categ-card-title'>Rum</p>
                         <p>Pirates favorite!</p>
@@ -33,7 +33,7 @@ function Categories () {
                         <p className='categ-card-title'>Wine</p>
                         <p>Wine placeholder text</p>
                     </div>
-                    <img className='categ-pic-sm' src={require(`../../images/Spirits/Whisky/low-res/jack-standard.jpg`)} alt={'Jack'}/>
+                    <img className='categ-pic-sm' src={require(`../../images/Wine/low-res/White/muni.jpg`)} alt={'Wine'}/>
                     <div className='check-out-btn categ-small-card-check'>Check it out</div>
                 </div>
             </div>
@@ -43,11 +43,11 @@ function Categories () {
                         <p className='categ-card-title'>Gift Cards</p>
                         <p>Gift baskets placeholder text</p>
                     </div>
-                    <img className='categ-pic-lg' src={require(`../../images/Covers/wine-cover.jpg`)} alt="" />
+                    <img className='categ-pic-lg' src={require(`../../images/Gift Cards/10off.png`)} alt="Gift Cards" />
                     <div className='check-out-btn categ-large-card-check'>Check it out</div>
                 </div>
                 <div className='categ-card-lg'>
-                    <img className='categ-pic-lg' src={require(`../../images/Covers/wine-cover.jpg`)} alt="" />
+                    <img className='categ-pic-lg' src={require(`../../images/Spirits/Whisky/low-res/red-label.jpg`)} alt="Whisky" />
                     <div>
                         <p className='categ-card-title'>Whisky</p>
                         <p>Whisky placeholder text</p>
