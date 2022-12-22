1bffc30589e5daf3ab219f7e37f5abf4e1e9b6e6
raduxc44 <raduxc44@gmail.com>
2022-12-22 17:40:20 +0200

    Updated nav for the new file structure

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -6,18 +6,84 @@ import Inventory from '../../data/inventory.json'
 function Nav () {
 
     //Functionality for the second desktop nav menu
-    const alreadyOpenedCateg = useRef(false)
+    const alreadyOpenedCateg = useRef(false);
+    const subCategories = useRef(0)
     const [selectedCategories, setSelectedCategories] = useState('spirits');
-    const shownCategories: string[] = useMemo(() => {
-        if(selectedCategories === 'spirits')            return Object.keys(Inventory.categories.spirits)
-        else if(selectedCategories === 'wines')         return Object.keys(Inventory.categories.wine)
-        else if(selectedCategories === 'champagne')     return Object.keys(Inventory.categories.champagne)
-        else if(selectedCategories === 'others')        return Object.keys(Inventory.categories.others)
-        else if(selectedCategories === 'gift-cards')    return Object.keys(Inventory.categories.giftCards)
-        else if(selectedCategories === 'on-sale')       return Object.keys(Inventory.categories.onSale)
-        else if(selectedCategories === 'daily-offer')   return Object.keys(Inventory.categories.dailyOffer)
+    const shownCategories: Object[] = useMemo(() => {
+        if(selectedCategories === 'spirits') {
+            let spiritsArr:Object[] = []
+            subCategories.current = 7;
+            Object.entries(Inventory.Items).forEach(item => {
+                if (item[1].category === 'Whisky' || 
+                    item[1].category === 'Vodka'  || 
+                    item[1].category === 'Rum'    ||
+                    item[1].category === 'Gin'    ||
+                    item[1].category === 'Liquor' ||
+                    item[1].category === 'Tequila'||
+                    item[1].category === 'Cognac' ) 
+                {spiritsArr.push(item[1])}
+            })
+            return spiritsArr 
+        }
+        else if(selectedCategories === 'wines') {
+            let winesArr: Object[] = []
+            subCategories.current = 3;
+            Object.entries(Inventory.Items).forEach(item => {
+                if(item[1].category === 'White-Wine' || 
+                    item[1].category === 'Red-Wine' || 
+                    item[1].category === 'Rose-Wine') 
+                {winesArr.push(item[1])}
+            })
+            return winesArr
+        }
+        else if(selectedCategories === 'champagne') {
+            let champagneArr: Object[] = []
+            subCategories.current = 0;
+            Object.entries(Inventory.Items).forEach(item => {
+                if(item[1].category === 'Champagne')
+                {champagneArr.push(item[1])}
+            })
+            return champagneArr
+        }
+        else if(selectedCategories === 'others') {
+            let othersArr: Object[] = []
+            subCategories.current = 2;
+            Object.entries(Inventory.Items).forEach(item => {
+                if (item[1].category === 'Beer' ||
+                    item[1].category === 'Beverage')
+                {othersArr.push(item[1])}
+            })
+            return othersArr
+        }
+        else if(selectedCategories === 'gift-cards') {
+            let giftCardsArr: Object[] = []
+            subCategories.current = 0;
+            Object.entries(Inventory.Items).forEach(item => {
+                if(item[1].category === 'Gift-Card')
+                {giftCardsArr.push(item[1])}
+            })
+            return giftCardsArr
+        }
+        // On sale and daily offer needs to be reworked ( separate property with boolean value? )
+        else if(selectedCategories === 'on-sale') {
+            let onSaleArr: Object[] = []
+            subCategories.current = 0;
+            Object.entries(Inventory.Items).forEach(item => {
+                if(item[1].category === 'On-Sale')
+                {onSaleArr.push(item[1])}
+            })
+            return onSaleArr
+        }
+        else if(selectedCategories === 'daily-offer') {
+            let dailyOfferArr: Object[] = []
+            subCategories.current = 0;
+            Object.entries(Inventory.Items).forEach(item => {
+                if(item[1].category === 'Daily-Offer') 
+                {dailyOfferArr.push(item[1])}
+            })
+            return dailyOfferArr
+        }
     }, [selectedCategories])!
-
     // Applies in and out animations on categories (mobile version only)
     // Delay refs are used to avoid animation bugging when pressing their buttons rapidly
     let categAnimationDelay = useRef(0)
@@ -114,48 +180,64 @@ function Nav () {
         primaryCategList.classList.add('animate__animated');
         secondCategList.classList.add('animate__animated');
         secondCategList.innerHTML = ''
-
-        for(let i = 0; i < shownCategories.length; i++) {
-            let listItemDiv: HTMLElement = document.createElement('div');
-            let listItem: HTMLElement = document.createElement('li');
-            let listItemImage: HTMLImageElement = document.createElement('img')
-            let listItemP: HTMLElement = document.createElement('p')
-            listItemP.innerText = shownCategories[i];
-            secondCategList.appendChild(listItemDiv);
-            listItemDiv.appendChild(listItem);
-            listItem.appendChild(listItemImage);
-            listItem.appendChild(listItemP)
-            listItemDiv.classList.add('secondary-categ-item')
-            if(selectedCategories === 'spirits') listItemDiv.classList.toggle('seven-categs');
-            else if(selectedCategories === 'wines') listItemDiv.classList.toggle('three-categs');
-            else if(selectedCategories === 'others') listItemDiv.classList.toggle('two-categs');
-            }
-            if(window.innerWidth < 1000) {
-                let listImagesArr: HTMLImageElement[] = Array.from(document.querySelectorAll('div.secondary-categ-item > li > img'))!;
-                let firstCategListElements: HTMLElement[] = Array.from(document.querySelectorAll('div.primary-categ-item'))!;
-                firstCategListElements[0].onclick = switchMenus;
-                firstCategListElements[1].onclick = switchMenus;
-                firstCategListElements[3].onclick = switchMenus;
-                if(selectedCategories === 'spirits') {
-                    listImagesArr[0].src = require(`../../images/Spirits/Whisky/mobile/jack-standard.webp`);
-                    listImagesArr[1].src = require(`../../images/Spirits/Vodka/mobile/absolut.webp`);
-                    listImagesArr[2].src = require(`../../images/Spirits/Cognac/mobile/hennessy.webp`);
-                    listImagesArr[3].src = require(`../../images/Spirits/Gin/mobile/koval.webp`);
-                    listImagesArr[4].src = require(`../../images/Spirits/Rum/mobile/the-kraken.webp`);
-                    listImagesArr[5].src = require(`../../images/Spirits/Tequila/mobile/jose-cuervo-reposado.webp`);
-                    listImagesArr[6].src = require(`../../images/Spirits/Liquor/mobile/disaronno.webp`)
-                
+        if( selectedCategories === 'spirits'||
+            selectedCategories === 'wines'  ||
+            selectedCategories === 'others'
+        ) {
+            for(let i = 0; i < subCategories.current; i++) {
+                let listItemDiv: HTMLElement = document.createElement('div');
+                let listItem: HTMLElement = document.createElement('li');
+                let listItemImage: HTMLImageElement = document.createElement('img')
+                let listItemP: HTMLElement = document.createElement('p')
+                secondCategList.appendChild(listItemDiv);
+                listItemDiv.appendChild(listItem);
+                listItem.appendChild(listItemImage);
+                listItem.appendChild(listItemP);
+                listItemDiv.classList.add('secondary-categ-item')
+                if(selectedCategories === 'spirits') listItemDiv.classList.toggle('seven-categs');
+                else if(selectedCategories === 'wines') listItemDiv.classList.toggle('three-categs');
+                else if(selectedCategories === 'others') listItemDiv.classList.toggle('two-categs');
                 }
-                else if(selectedCategories === 'wines') {                    
-                    listImagesArr[0].src = require(`../../images/Wine/mobile/Red/samtrot-spatlese.webp`);
-                    listImagesArr[1].src = require(`../../images/Wine/mobile/White/muni.webp`);
-                    listImagesArr[2].src = require(`../../images/Wine/mobile/Rose/cave-amadeu.webp`);
+                if(window.innerWidth < 1000) {
+                    let listCategoryNamesArr: HTMLElement[] = Array.from(document.querySelectorAll('div.secondary-categ-item > li > p'))
+                    let listImagesArr: HTMLImageElement[] = Array.from(document.querySelectorAll('div.secondary-categ-item > li > img'))!;
+                    let firstCategListElements: HTMLElement[] = Array.from(document.querySelectorAll('div.primary-categ-item'))!;
+                    firstCategListElements[0].onclick = switchMenus;
+                    firstCategListElements[1].onclick = switchMenus;
+                    firstCategListElements[3].onclick = switchMenus;
+                    if(selectedCategories === 'spirits') {
+                        listCategoryNamesArr[0].innerText = 'Whisky'
+                        listImagesArr[0].src = require(`../../images/Whisky/mobile/jack-standard.webp`);
+                        listCategoryNamesArr[1].innerText = 'Vodka'
+                        listImagesArr[1].src = require(`../../images/Vodka/mobile/absolut.webp`);
+                        listCategoryNamesArr[2].innerText = 'Cognac'
+                        listImagesArr[2].src = require(`../../images/Cognac/mobile/hennessy.webp`);
+                        listCategoryNamesArr[3].innerText = 'Gin'
+                        listImagesArr[3].src = require(`../../images/Gin/mobile/koval.webp`);
+                        listCategoryNamesArr[4].innerText = 'Rum'
+                        listImagesArr[4].src = require(`../../images/Rum/mobile/the-kraken.webp`);
+                        listCategoryNamesArr[5].innerText = 'Tequila'
+                        listImagesArr[5].src = require(`../../images/Tequila/mobile/jose-cuervo-reposado.webp`);
+                        listCategoryNamesArr[6].innerText = 'Liquor'
+                        listImagesArr[6].src = require(`../../images/Liquor/mobile/disaronno.webp`)
+                    }
+                    else if(selectedCategories === 'wines') {    
+                        listCategoryNamesArr[0].innerText = 'Red Wine'                
+                        listImagesArr[0].src = require(`../../images/Red-Wine/mobile/samtrot-spatlese.webp`);
+                        listCategoryNamesArr[1].innerText = 'White Wine'
+                        listImagesArr[1].src = require(`../../images/White-Wine/mobile/muni.webp`);
+                        listCategoryNamesArr[2].innerText = 'Rose Wine'
+                        listImagesArr[2].src = require(`../../images/Rose-Wine/mobile/cave-amadeu.webp`);
+                    }
+                    else if(selectedCategories === 'others') {
+                        listCategoryNamesArr[0].innerText = 'Beer'
+                        listImagesArr[0].src = require(`../../images/Beer/mobile/desperados.webp`);
+                        listCategoryNamesArr[1].innerText = 'Beverages'
+                        listImagesArr[1].src = require(`../../images/Beverage/mobile/coca-cola.webp`);
+                    }
                 }
-                else if(selectedCategories === 'others') {
-                    listImagesArr[0].src = require(`../../images/Others/Beer/mobile/desperados.webp`);
-                    listImagesArr[1].src = require(`../../images/Others/Beverages/mobile/coca-cola.webp`);
-                }
-            }
+        }
+        
     }, [selectedCategories, shownCategories])
 
     function searchFunc () {
@@ -211,7 +293,7 @@ function Nav () {
                             alreadyOpenedCateg.current = true;
                             }}>
                             <li>
-                                <img src={require(`../../images/Spirits/Whisky/mobile/jack-honey.webp`)} alt="Whisky" />
+                                <img src={require(`../../images/Whisky/mobile/jack-honey.webp`)} alt="Whisky" />
                                 <p>Spirits</p>
                             </li>
                         </div>
@@ -220,7 +302,7 @@ function Nav () {
                             alreadyOpenedCateg.current = true;
                             }}>
                             <li>
-                                <img src={require(`../../images/Wine/mobile/Red/samtrot-spatlese.webp`)} alt="Whisky" />
+                                <img src={require(`../../images/Red-Wine/mobile/samtrot-spatlese.webp`)} alt="Whisky" />
                                 <p>Wine</p>
                             </li>
                         </div>
@@ -238,7 +320,7 @@ function Nav () {
                             alreadyOpenedCateg.current = true;
                             }}>
                             <li>
-                                <img src={require(`../../images/Others/Beer/mobile/desperados.webp`)} alt="Others" />
+                                <img src={require(`../../images/Beer/mobile/desperados.webp`)} alt="Others" />
                                 <p>Others</p>
                             </li>
                         </div>
