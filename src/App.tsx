db757d7bea5b8886ce0f9c2dab8cc5c44b048a49
raduxc44 <raduxc44@gmail.com>
2023-01-20 22:12:21 +0200

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
 import './App.css';
-import "slick-carousel/slick/slick.css"
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/home';
 import IndividualProdPage from './pages/individualProdPage';
-import ShoppingCart from './pages/shoppingCart';
 import { Item } from './data/types';
 import { SelectedProdContext } from './Contexts/selectedProductContext';
 
@@ -25,7 +23,6 @@ function App() {
             <Route 
             path={`/product/:productId`} 
             element={<IndividualProdPage />} />
-          <Route path='/shopping-cart' element= {<ShoppingCart />}></Route>
         </Routes>
       </BrowserRouter>
     </SelectedProdContext.Provider>
--- src/data/comments.js
+++
@@ -1,8 +0,0 @@
-// "talisker":{
-//     "name":"Talisker",
-//     "imageTag":"talisker",
-//     "quantity":"0.7L",
-//     "strength":"40%",
-//    "price":255,
-//      "flavour: !!!!!!!"
-// },
