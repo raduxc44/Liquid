3d45cb47a06c90e433828e438f49ec7a40700d65
raduxc44 <raduxc44@gmail.com>
2023-01-28 00:04:24 +0200

    FilteredProducts

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,13 @@ import Home from './pages/home';
 import IndividualProdPage from './pages/individualProdPage';
 import { Item } from './data/types';
 import { SelectedProdContext } from './Contexts/selectedProductContext';
+import { SelectedFilterContext } from './Contexts/selectedFilterContext';
+import FilteredProducts from './pages/filteredProductsPage';
 
 function App() {
 
   const [selectedProductToShow, setSelectedProductToShow] = useState<Item | null>(null);
+  const [selectedFilter, setSelectedFilter] = useState<Item[] | []>([]);
   const [quantityValue, setQuantityValue] = useState<number>(1);
   
 
@@ -16,17 +19,25 @@ function App() {
     <SelectedProdContext.Provider value={{
       selectedProductToShow, setSelectedProductToShow,
       quantityValue, setQuantityValue
-      }}>
+    }}>
+    <SelectedFilterContext.Provider value={{
+      selectedFilter, setSelectedFilter
+    }}>
       <BrowserRouter>
         <Routes>
             <Route index element= {<Home/>}></Route>
             <Route 
             path={`/product/:productId`} 
             element={<IndividualProdPage />} />
+            <Route
+            path={`category/:categoryId`}
+            element={<FilteredProducts />}
+            />
         </Routes>
       </BrowserRouter>
+    </SelectedFilterContext.Provider>
     </SelectedProdContext.Provider>
-
+    
   );  
 }
 
